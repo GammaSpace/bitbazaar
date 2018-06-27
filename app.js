3699a0db13c3b014dc6017ff6537fb46d8cd25e7
Jennie Robinson Faber <jennie@cipritine.local>
2018-06-27 11:43:14 -0400

    Preppring for lineup announcement

---
+++ app.js
@@ -0,0 +1,38 @@
+const htmlStandards = require('reshape-standard')
+const cssStandards = require('spike-css-standards')
+const jsStandards = require('spike-js-standards')
+const pageId = require('spike-page-id')
+const sugarml = require('sugarml')
+const sugarss = require('sugarss')
+const env = require('dotenv').config()
+
+const SpikeDatoCMS = require('spike-datocms')
+const postcssMixins = require('postcss-mixins')
+const locals = {}
+
+const Dato = new SpikeDatoCMS({
+  addDataTo: locals,
+  token: process.env.dato_api_key,
+  models: [{
+    name: 'project'
+  }],
+  json: 'data.json'
+})
+
+module.exports = {
+  devtool: 'source-map',
+  matchers: { html: '*(**/)*.sgr', css: '*(**/)*.css' },
+  ignore: ['**/layout.sgr', '**/_*', '**/.*', 'readme.md', 'yarn.lock', 'package-lock.json'],
+  reshape: htmlStandards({
+    parser: sugarml,
+    locals: (ctx) => { return Object.assign(locals)},
+    minify: env === 'production'
+  }),
+  postcss: cssStandards({
+    warnForDuplicates: env !== 'production',
+    appendPlugins: postcssMixins()
+  }),
+  babel: jsStandards(),
+  plugins: [ Dato ]
+
+}
