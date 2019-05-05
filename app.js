1a586678526ad9c4165ea51b9b6c9d9663054753
Jennie Robinson Faber <jennie@cipritine.local>
2019-05-05 17:19:23 -0400

    Updates to ready for lineup announcement

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ module.exports = {
         graphql: {
           url: 'https://graphql.datocms.com/',
           query: ` {
-            allProjects(filter: {editions: {eq: "950754"}}, orderBy: [id_DESC], first: 100) {
+            allProjects(filter: {editions: {eq: "950754"}}, orderBy: [name_ASC], first: 100) {
               name
               category
               link
